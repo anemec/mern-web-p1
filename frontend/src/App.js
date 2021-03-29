565d944e290a5e5713b574b001d2b0759e061af8
Andrew Nemec <andrewnemec2@gmail.com>
2021-03-29 15:57:12 -0500

    Initial setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <main className="py-1 mc"></main>
+    </Router>
+  );
+};
+
+export default App;
