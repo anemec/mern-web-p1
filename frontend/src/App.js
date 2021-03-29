1bafd197289f81aac446503707b7d18a6c920361
Andrew Nemec <andrewnemec2@gmail.com>
2021-03-29 17:09:27 -0500

    Another commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 import Header from "./components/Header";
 const App = () => {
   return (
     <Router>
       <Header />
-      <main className="py-1 mc"></main>
+      <main className="py-3">
+        <Container className="mc">
+          <h1>Hello</h1>
+        </Container>
+      </main>
     </Router>
   );
 };
