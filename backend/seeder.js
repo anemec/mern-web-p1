f383507dec6ea0cb5622582ed1742c9b92eb70cf
anemec-f <78433068+anemec-f@users.noreply.github.com>
2021-04-10 17:32:04 -0500

    AN - Added backend user logic

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 import Box from "./models/boxModel.js";
+import User from "./models/userModel.js";
 import connectDB from "./config/db.js";
 import { boxes } from "./data/boxes.js";
+import users from "./data/users.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -11,7 +13,9 @@ connectDB();
 const importData = async () => {
   try {
     await Box.deleteMany();
+    await User.deleteMany();
     await Box.insertMany(boxes);
+    await User.insertMany(users);
     console.log("Data imported!");
     process.exit();
   } catch (e) {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Header from './components/Header';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Header from "./components/Header";
 
-import BoxScreen from './screens/BoxScreen';
-import HomeScreen from './screens/HomeScreen';
+import BoxScreen from "./screens/BoxScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <main className='py-3'>
-        <Container className='mc'>
-          <Route path='/' exact>
+      <main className="p-3">
+        <Container className="mc" fluid>
+          <Route path="/" exact>
             <HomeScreen />
           </Route>
-          <Route path='/box/:id'>
+          <Route path="/box/:id">
             <BoxScreen />
           </Route>
         </Container>
